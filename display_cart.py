c99354d0e58f0342c2abb977a824625f1e240eb3
prettysmilefordevil <prettysmilefordevil@gmail.com>
2017-10-25 00:37:01 +0300

    new update 3

--- display_cart.py
+++ display_cart.py
@@ -11,7 +11,7 @@ def display_schedule(cartoons):
 		mon='ноября'
 	for cartoon in cartoons:
 		if cartoon['c_time'].minute==0:
-			output = datetime.strftime(today,"%d")+mon+'\n{0}:0{1}  {2} ({3}) \n {4} \n\n'.format(cartoon['c_time'].hour,cartoon['c_time'].minute,
+			output = today.strftime("%d")+mon+'\n{0}:0{1}  {2} ({3}) \n {4} \n\n'.format(cartoon['c_time'].hour,cartoon['c_time'].minute,
 														cartoon['c_name'], cartoon['c_age'],cartoon['c_place'])
 			if not cartoon['c_url']=='':
 				output=output+'('+cartoon['c_url']+')'
