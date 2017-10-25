e7f9a86697e9c72827093a12473ac4abb22b9543
prettysmilefordevil <prettysmilefordevil@gmail.com>
2017-10-25 11:36:18 +0300

    semi-final

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-token='433009374:AAHRoyKzc8tcvTEDQf6yqvhgos58i0736Xg'
+token='479319204:AAE6FqVxpJ-WBJcUo_O6TlevMCaqaV_tbgI'
 database_url='postgres://hxjnfvkl:7lJ-VibsOYw0EJU8TLd022OK-56Bc4HT@elmer.db.elephantsql.com:5432/hxjnfvkl'
